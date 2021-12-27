1a5f3c669f80dc88551c9d39a6f712a30c4a0cca
reosong <ambition_1018@naver.com>
2021-12-27 17:03:50 +0900

    first commit

---
+++ src/main/java/project/youngsinsa/YoungsinsaApplication.java
@@ -0,0 +1,13 @@
+package project.youngsinsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungsinsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoungsinsaApplication.class, args);
+	}
+
+}
