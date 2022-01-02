67ba9c0c6b8f43ab2ac3fe8e8ea5f7394fb654a9
reosong <ambition_1018@naver.com>
2022-01-02 19:22:46 +0900

    1.2

--- src/main/java/project/youngsinsa/member/memberRepository/MyMemberRepository.java
+++ src/main/java/project/youngsinsa/member/memberRepository/MyMemberRepository.java
@@ -5,8 +5,24 @@ import org.springframework.context.annotation.Bean;
 
 
 public class MyMemberRepository implements MemberReopository{
+
+    public MyMemberRepository() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/tesla";
+            String id = "root";
+            String password = "1234";
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void save() {
+        String sql = "insert into ";
+        try{
+
+        }catch ()
 
     }
 
